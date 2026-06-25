47e008656f01b27756f6407bef773e7f34d58db5
srija-1189 <knagasrija01@gmail.com>
2026-06-25 10:55:08 +0530

    Fixed map crash

--- public/js/map.js
+++ public/js/map.js
@@ -2,24 +2,32 @@ console.log("map loaded");
 
 const mapDiv = document.getElementById("map");
 
-const coordinates =
-JSON.parse(mapDiv.dataset.coordinates);
+if (mapDiv) {
 
-const map = L.map("map").setView(
-    [coordinates[1],coordinates[0]],
-    13
-);
+    const coordinates = JSON.parse(
+        mapDiv.dataset.coordinates || "[]"
+    );
 
-L.tileLayer(
-'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-{
- attribution:'© OpenStreetMap'
+    // stop if coordinates don't exist
+    if (coordinates.length === 2) {
+
+        const map = L.map("map").setView(
+            [coordinates[1], coordinates[0]],
+            13
+        );
+
+        L.tileLayer(
+            'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            {
+                attribution: '© OpenStreetMap'
+            }
+        ).addTo(map);
+
+        L.marker(
+            [coordinates[1], coordinates[0]]
+        )
+        .addTo(map)
+        .bindPopup("Listing Location")
+        .openPopup();
+    }
 }
-).addTo(map);
-
-L.marker(
-[coordinates[1],coordinates[0]]
-)
-.addTo(map)
-.bindPopup("Listing Location")
-.openPopup();
