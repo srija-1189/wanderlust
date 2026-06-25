d8e4e4fde681656a3237059d3a55cbff2cb48c22
srija-1189 <knagasrija01@gmail.com>
2026-06-25 11:26:50 +0530

    Fixed Render port binding

--- app.js
+++ app.js
@@ -113,8 +113,9 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
+
 
 
 
